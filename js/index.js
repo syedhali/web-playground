f526da6e27ca2a88de66db383ecb2a73fa01320b
Haris Ali <syedhali07@gmail.com>
2020-04-04 14:53:22 -0500

    Will it work

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
+  console.log("Just some random logging"   )
   element.innerHTML = _.join(["Hi", "mom!"], " ");
 
   return element;
