6cdcb0e4a38abdd004c87168d58aaaa0de24ab52
Haris Ali <syedhali07@gmail.com>
2020-04-04 14:45:03 -0500

    should only format selected files

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./js/index.js",
   mode: "production",
+
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
