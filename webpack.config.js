2993ff7292f3d89e9ff01ea8b6076bbfdb904b77
Haris Ali <syedhali07@gmail.com>
2020-04-04 14:55:39 -0500

    should format webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require("path");
 
+console.log("Hi mom");
+
 module.exports = {
   entry: "./js/index.js",
   mode: "production",
-
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
