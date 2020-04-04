611fa6b19c362057a5d8257a77bb3913761d070d
Haris Ali <syedhali07@gmail.com>
2020-04-04 14:34:40 -0500

    reset hooks path

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(["Hi",         "mom!"], " ");
+  element.innerHTML = _.join(["Hi", "mom!"], " ");
 
   return element;
 }
